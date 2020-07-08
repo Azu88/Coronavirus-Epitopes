fc5ee5d22d49fb48be45def873aaa3a88a6d330f
Nika Zaslavsky <nikazaslavsky@gmail.com>
2020-07-07 22:03:49 -0400

    implemented simple count ilp

--- ilp.py
+++ ilp.py
@@ -6,12 +6,13 @@ import pickle
 
 def main():
     ## parameters
-    max_peptides = 2
+    max_peptides = 5
     affinity_threshold = 0.638
-    
+
+    path = 'C:\\Users\\Nika\\Downloads\\Organized\\Coronavirus-Epitopes\\'
+        
     
     ## load data
-    path = 'C:\\Users\\Nika\\Downloads\\Organized\\Coronavirus-Epitopes\\'
     summary = pd.read_pickle(path + 'summary.pkl')
     rows = summary.index.size
     
@@ -31,22 +32,42 @@ def main():
     for peptide in summary.index:
         vaccine_size.SetCoefficient(x[peptide], 1)
         
-    spike_covered = solver.Constraint(-1, -1, "spike_covered")
+    S1_covered = solver.Constraint(-rows, -1, "S1_covered")
     for peptide in summary.index:
         protein = summary.at[peptide, ("Features", "protein")]
-        spike_covered.SetCoefficient(x[peptide], -1 if protein in {"S1", "S2"} else 0)
+        S1_covered.SetCoefficient(x[peptide], -1 if protein == "S1" else 0)
 
-    sars_cov_covered = solver.Constraint(-1, -1, "sars_cov_covered")
+    S2_covered = solver.Constraint(-rows, -1, "S2_covered")
+    for peptide in summary.index:
+        protein = summary.at[peptide, ("Features", "protein")]
+        S2_covered.SetCoefficient(x[peptide], -1 if protein == "S2" else 0)
+
+    E_covered = solver.Constraint(-rows, -1, "E_covered")
+    for peptide in summary.index:
+        protein = summary.at[peptide, ("Features", "protein")]
+        E_covered.SetCoefficient(x[peptide], -1 if protein == "E" else 0)
+
+    M_covered = solver.Constraint(-rows, -1, "M_covered")
+    for peptide in summary.index:
+        protein = summary.at[peptide, ("Features", "protein")]
+        M_covered.SetCoefficient(x[peptide], -1 if protein == "M" else 0)
+
+    N_covered = solver.Constraint(-rows, -1, "N_covered")
+    for peptide in summary.index:
+        protein = summary.at[peptide, ("Features", "protein")]
+        N_covered.SetCoefficient(x[peptide], -1 if protein == "N" else 0)
+
+    SARS_CoV1_covered = solver.Constraint(-rows, -1, "SARS_CoV1_covered")
     for peptide in summary.index:
         covered = summary.at[peptide, ("Features", "In_SARS_Cov1")]
-        sars_cov_covered.SetCoefficient(x[peptide], -1 if covered else 0)
+        SARS_CoV1_covered.SetCoefficient(x[peptide], -1 if covered else 0)
 
 
     # objective
     objective = solver.Objective()
     for peptide in summary.index:
         df = summary.loc[peptide, "Genotypes"]
-        objective.SetCoefficient(x[peptide], int(df.min()))
+        objective.SetCoefficient(x[peptide], int(df.sum()))
     objective.SetMaximization()
 
 
@@ -67,12 +88,12 @@ def main():
             vaccine.add(peptide)
             print(peptide)
         
-    # check
     for peptide in vaccine:
         print("\n")
         print(peptide)
         print("protein: " + summary.at[peptide, ("Features", "protein")])
         print("in SARS-Cov1: " + str(summary.at[peptide, ("Features", "In_SARS_Cov1")]))
+        print("genotypes covered: " + str(summary.loc[peptide, "Genotypes"].sum()))
 
 if __name__ == '__main__':
     main()
